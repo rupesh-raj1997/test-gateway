eb810e8cf0a1fcc023ff9d60d9d747caaee62f76
zigmint <rupeshr053@gmail.com>
2025-07-17 01:32:34 +0530

    feat: add nodemon configuration and implement comment management endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { COMMENTS_SERVICE } from './utils/constants';
 import { ClientProxy } from '@nestjs/microservices';
@@ -17,9 +17,41 @@ export class AppController {
   @Get('comments-health')
   async commentsHealth() {
     const resp = await this.client.send('comments.health', {})
-    console.log({
-      resp
-    })
+    return resp;
+  }
+
+
+  @Post('comment/')
+  async createNewComment(@Body() body: any) {
+    try {
+      const resp = this.client.send({ cmd: 'comment.create' }, body);
+      console.log("POST COMMENT resp", resp)
+      return resp;
+    } catch (err) {
+      console.log("POST COMMENT err", err)
+    }
+  }
+
+  @Get('comment/:id')
+  async findOneComment(@Param('id') id: string) {
+    return this.client.send({ cmd: 'comment.findOne' }, id);
+  }
+
+  @Patch('comment/:id')
+  async updateComment(@Param('id') id: string, @Body() body: any) {
+    const resp = await this.client.send({ cmd: 'comment.update' }, { id, ...body });
+    return resp;
+  }
+
+  @Delete('comment/:id')
+  async deleteComment(@Param('id') id: string) {
+    const resp = await this.client.send({ cmd: 'comment.remove' }, id);
+    return resp;
+  }
+
+  @Get('comment/thread/:id')
+  async commentThread(@Param('id') id: string) {
+    const resp = await this.client.send({ cmd: 'comment.thread' }, id);
     return resp;
   }
 }
